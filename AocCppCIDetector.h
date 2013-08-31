f441b0ba58e1b34f9cbfe8bc1538540c7379300b
Philip <philip2@Philips-MacBook-Pro.local>
2013-08-31 16:30:10 -0700

    Improved comment about the "PIMPL pattern".

--- AocCppCIDetector.h
+++ AocCppCIDetector.h
@@ -85,7 +85,7 @@ namespace Aoc
         
     private:
         
-        // The data members of this class are hidden in the .cpp file.
+        // Details of the class' data are hidden in the .cpp file.
         
         class Imp;
         std::unique_ptr<Imp> _m;
